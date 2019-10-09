27b5944e37fc64022700aa10464a99084c87a113
sengoku <sengokyu+gh@gmail.com>
2019-10-09 23:39:55 +0900

    クエリパラメータがあるときは読み込むようにしました。

--- projects/frontend-hokanchan/src/app/suggestion/suggestion.component.ts
+++ projects/frontend-hokanchan/src/app/suggestion/suggestion.component.ts
@@ -1,24 +1,39 @@
 import { Location } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { SuggestionService } from './suggestion.service';
+import { ActivatedRoute } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-suggestion',
   templateUrl: './suggestion.component.html'
 })
-export class SuggestionComponent {
+export class SuggestionComponent implements OnInit {
   readonly suggestions$ = this.suggestionService.suggestions$;
   queryWord = '';
 
   constructor(
+    private route: ActivatedRoute,
     private location: Location,
     private suggestionService: SuggestionService
   ) {}
 
+  ngOnInit(): void {
+    this.route.queryParamMap
+      .pipe(filter(params => params.has('q')))
+      .subscribe(params => {
+        this.queryWord = params.get('q');
+        this.showResult();
+      });
+  }
+
   onSubmit(): void {
     this.location.go('.', `q=${this.queryWord}`);
+    this.showResult();
+  }
 
+  private showResult(): void {
     this.suggestionService.update(this.queryWord);
   }
 }
